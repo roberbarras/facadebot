5e7c50da85d9d5214fa396c3f8bc4475c7d96c2d
roberbarras <roberbarras@gmail.com>
2021-03-15 21:08:39 +0100

    Add kafka producer feature

--- src/main/java/org/telegram/bot/facadebot/config/KakfaConfiguration.java
+++ src/main/java/org/telegram/bot/facadebot/config/KakfaConfiguration.java
@@ -1,14 +1,17 @@
 package org.telegram.bot.facadebot.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.telegram.bot.facadebot.model.TelegramMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,11 +38,26 @@ public class KakfaConfiguration {
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, TelegramMessage> customFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, TelegramMessage> customConsumerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, TelegramMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         factory.setBatchListener(true);
         return factory;
+    }
+
+    public ProducerFactory<String, Update> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Update> customProducerFactory() {
+        return new KafkaTemplate<>(producerFactory());
     }
 
 }
