7f1d743bfdf87a865acd3486d2c861bdb1b1ac74
roberbarras <roberbarras@gmail.com>
2021-03-22 21:10:39 +0100

    Optimize imports

--- src/main/java/org/telegram/bot/facadebot/messenger/Bot.java
+++ src/main/java/org/telegram/bot/facadebot/messenger/Bot.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import org.telegram.bot.facadebot.model.TelegramMessage;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
